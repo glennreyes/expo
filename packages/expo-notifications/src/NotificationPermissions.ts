013c76c140543ef148ded54f1549f8872ceebbb3
Stanisław Chmiela <sjchmiela@gmail.com>
2020-01-20 16:33:20 +0100

    [expo-notifications] Implement permissions requesting methods

---
+++ packages/expo-notifications/src/NotificationPermissions.ts
@@ -0,0 +1,24 @@
+import NotificationPermissionsModule, {
+  NotificationPermissionsRequest,
+} from './NotificationPermissionsModule';
+
+export {
+  AndroidImportance,
+  IosAlertStyle,
+  IosAllowsPreviews,
+  IosAuthorizationStatus,
+} from './NotificationPermissionsModule';
+
+export async function getPermissionsAsync() {
+  return await NotificationPermissionsModule.getPermissionsAsync();
+}
+
+export async function requestPermissionsAsync(permissions?: NotificationPermissionsRequest) {
+  return await NotificationPermissionsModule.requestPermissionsAsync(
+    permissions ?? {
+      allowAlert: true,
+      allowBadge: true,
+      allowSound: true,
+    }
+  );
+}
